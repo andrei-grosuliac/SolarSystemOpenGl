d61ba570c631d9171239d8ef21be08318a615416
Andrei Grosuliac <agrosuliac@gmail.com>
2023-03-27 19:25:33 -0400

    initial commit

---
+++ SolarSystem/src/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW, GLEW & GLM Test", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+   // glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Test GLM
+    glm::vec3 a(1.0f, 2.0f, 3.0f);
+    glm::vec3 b(4.0f, 5.0f, 6.0f);
+    glm::vec3 result = a + b;
+
+    std::cout << "GLM vector addition result: (" << result.x << ", " << result.y << ", " << result.z << ")" << std::endl;
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Clean up and exit
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
